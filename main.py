0cdc6238e4b31ccdc2a6aa675c36d0bf8e6120e5
katariya <katariya0116@gmail.com>
2020-04-19 16:45:44 +0900

    ファイル読み込み部分の対応

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, caption="Hello Pyxel")
+        pyxel.load("assets/test000.pyxres")
         self.x = 0
         pyxel.run(self.update, self.draw)
 
@@ -16,5 +17,5 @@ class App:
         pyxel.cls(0)
         pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
         pyxel.rect(self.x, 0, 7, 7, 9)
-        #pyxel.blt(61, 66, 0, 0, 0, 38, 16)
+        pyxel.blt(61, 66, 0, 0, 0, 32, 32)
 App()
